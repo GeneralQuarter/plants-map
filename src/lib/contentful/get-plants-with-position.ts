efc96baf322a075fff48f2a49230d7fa502a201f
GeneralQuarter <x1q.g1x@gmail.com>
2024-10-28 16:46:35 +0100

    fix: only 1k plants loaded + bump deps

--- src/lib/contentful/get-plants-with-position.ts
+++ src/lib/contentful/get-plants-with-position.ts
@@ -1,14 +1,29 @@
-import { ContentfulClientApi } from 'contentful';
+import { ContentfulClientApi, EntriesQueries } from 'contentful';
 import { entryCollectionToPaginatedResult } from './entry-collection-to-paginated-result';
 import { entryToPlant } from './entry-to-plant';
 import { PlantEntrySkeleton } from './plant.entry-skeleton';
 
+const chunkSize = 1000;
+
 export async function getPlantsWithPosition(client: ContentfulClientApi<undefined>) {
-  const collection = await client.withoutUnresolvableLinks.getEntries<PlantEntrySkeleton, 'fr'>({
+  const baseQuery: EntriesQueries<PlantEntrySkeleton, 'WITHOUT_UNRESOLVABLE_LINKS'> = {
     content_type: 'plant',
-    limit: 1000,
+    limit: chunkSize,
     'fields.position[exists]': true
-  });
+  };
+
+  const collection = await client.withoutUnresolvableLinks.getEntries<PlantEntrySkeleton, 'fr'>(baseQuery);
+
+  const nbCalls = Math.ceil(collection.total / chunkSize) - 1;
+
+  for (let i = 1; i <= nbCalls; i++) {
+    const chunkCollection = await client.withoutUnresolvableLinks.getEntries<PlantEntrySkeleton, 'fr'>({
+      ...baseQuery,
+      skip: i * chunkSize
+    });
+
+    collection.items = collection.items.concat(chunkCollection.items);
+  }
 
   return entryCollectionToPaginatedResult(collection, entryToPlant);
 }
